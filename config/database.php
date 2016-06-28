1b25ccd6439b0694e70c74741624c33b3bb02dbe
WalterJunior <walter.junior@projetusti.com.br>
2016-06-27 21:37:37 -0300

    Upload to Heroku

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -70,9 +70,9 @@ return [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd706dpj0b7l7no'),
+            'username' => env('DB_USERNAME', 'ggoobmnhftmqqt'),
+            'password' => env('DB_PASSWORD', 'eqts_D7pgOyosD_G20TJ11MKMX'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
